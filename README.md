a65380ddbca425dc95110c55e45ae8608df5f0ea
georgi-kr <gkrastev9715@abv.bg>
2022-11-03 17:46:54 +0200

    add example

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # node-io
+
 Simple node js app to modify files (csv, json)
+
+> - example to transform csv to json array
+
+```javascript
+const promise = new Promise((resolve, reject) => {
+  fs.createReadStream(
+    __dirname + '/../files-io/account-levelisations.csv',
+  ).pipe(
+    parse(
+      {
+        columns: true,
+        trim: true,
+        delimiter: ',',
+      },
+      (err, records) => resolve(records),
+    ),
+  );
+});
+
+const array = await promise;
+console.log(array);
+```
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
 import fs from 'fs';
-import csv from 'csv-parse';
+import path, { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { parse } from 'csv-parse';
+
+// get the current directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // main fn
-(() => {
-  // execute main fn body
-  console.log('Main fn');
+(async () => {
+  const promise = new Promise((resolve, reject) => {
+    fs.createReadStream(
+      __dirname + '/../files-io/account-levelisations.csv',
+    ).pipe(
+      parse(
+        {
+          columns: true,
+          trim: true,
+          delimiter: ',',
+        },
+        (err, records) => resolve(records),
+      ),
+    );
+  });
+
+  const array = await promise;
+  console.log(array);
 })();
