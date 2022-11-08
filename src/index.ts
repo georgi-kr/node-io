f72428913d3dc7053b55d7fdfc95f391b9454370
georgi-kr <gkrastev9715@abv.bg>
2022-11-08 15:18:28 +0200

    add example usage

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path, { resolve } from 'path';
 import { fileURLToPath } from 'url';
 import { parse } from 'csv-parse';
+import _ from 'lodash';
 
 // get the current directory
 const __filename = fileURLToPath(import.meta.url);
@@ -9,21 +10,44 @@ const __dirname = path.dirname(__filename);
 
 // main fn
 (async () => {
-  // const promise = new Promise((resolve, reject) => {
-  //   fs.createReadStream(
-  //     __dirname + '/../files-io/account-levelisations.csv',
-  //   ).pipe(
-  //     parse(
-  //       {
-  //         columns: true,
-  //         trim: true,
-  //         delimiter: ',',
-  //       },
-  //       (err, records) => resolve(records),
-  //     ),
-  //   );
-  // });
+  const promiseOriginal: Promise<any[]> = new Promise((resolve, reject) => {
+    fs.createReadStream(__dirname + '/../files-io/sorted-list.csv').pipe(
+      parse(
+        {
+          columns: true,
+          trim: true,
+          delimiter: ',',
+        },
+        (err, records) => resolve(records),
+      ),
+    );
+  });
 
-  // const array = await promise;
-  // console.log(array);
+  const promiseNewest: Promise<any[]> = new Promise((resolve, reject) => {
+    fs.createReadStream(
+      __dirname + '/../files-io/sorted-no-installations.csv',
+    ).pipe(
+      parse(
+        {
+          columns: true,
+          trim: true,
+          delimiter: ',',
+        },
+        (err, records) => resolve(records),
+      ),
+    );
+  });
+
+  const original = await promiseOriginal;
+  const newest = await promiseNewest;
+
+  fs.writeFileSync(
+    __dirname + '/../files-io/old.json',
+    JSON.stringify(original[2], null, 2),
+  );
+  fs.writeFileSync(
+    __dirname + '/../files-io/new.json',
+    JSON.stringify(newest[2], null, 2),
+  );
+  console.log('finished');
 })();
